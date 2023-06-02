4c7f7df89f1b85d262be9a8e48b234f07ee82b5b
Hadi-bakhshi <hadibakhshi277@gmail.com>
2023-06-02 15:05:02 +0330

    feat: add custom colors
    add my own custom colors to the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+// const colors = require('tailwindcss/colors');
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,13 +9,36 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // Primary
+        primary: '#2151DA',
+        onPrimary: '#FFFFFF',
+        primaryContainer: '#DCE1FF',
+        onPrimaryContainer: '#001551',
+        // Secondary
+        secondary: '#5A5D72',
+        onSecondary: '#FFFFFF',
+        secondaryContainer: '#DEE1F9',
+        onSecondaryContainer: '#171B2C',
+        // Tertiary
+        tertiary: '#75546F',
+        onTertiary: '#FFFFFF',
+        tertiaryContainer: '#FFD7F5',
+        onTertiaryContainer: '#2C1229',
+        // Error
+        error: '#BA1A1A',
+        onError: '#FFFFFF',
+        errorContainer: '#FFDAD6',
+        onErrorContainer: '#410002',
+        // background
+        background: '#FEFBFF',
+        onBackground: '#1B1B1F',
       },
     },
   },
   plugins: [],
-}
+};
