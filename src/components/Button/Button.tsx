82b42938c26e1c3b072ea24b3f1f29e094f6cacc
Hadi-bakhshi <hadibakhshi277@gmail.com>
2023-06-04 11:55:21 +0330

    feat: add elevated type button
    add elevated style button which has its own styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,7 +12,7 @@ const Button: React.FC<ButtonProps> = ({
       case 'filled':
         return 'bg-primary text-white';
       case 'elevated':
-        return;
+        return 'text-primary shadow-elevated hover:shadow-[0_1px_2px_rgba(0,0,0,0.3),0_2px_6px_2px_rgba(0,0,0,0.15)] active:shadow-[0_1px_2px_rgba(0,0,0,0.3),0_1px_3px_1px_rgba(0,0,0,0.15)]';
       case 'outlined':
         return;
       case 'text':
@@ -26,7 +26,7 @@ const Button: React.FC<ButtonProps> = ({
     <button
       className={`flex items-center justify-center rounded-full px-4 py-2 font-medium ${addVariantStyles(
         variant
-      )} ${styles.button}`}
+      )} ${variant === 'filled' && styles.button}`}
       onClick={onClick}
     >
       {icon && <span className='mr-2'>{icon}</span>}
